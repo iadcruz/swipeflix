3f41ad249b73e8f963bccaf7405fc3c8a6a6cc62
iadcruz <iadcruzsa21@gmail.com>
2025-08-01 19:03:14 -0400

    Allow iframe embedding

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+    async headers() {
+        return [
+        {
+            source: '/(.*)',
+            headers: [
+            {
+                key: 'X-Frame-Options',
+                value: 'ALLOWALL',
+            },
+            {
+                key: 'Content-Security-Policy',
+                value: "frame-ancestors *",
+            },
+            ],
+        },
+        ];
+    },
     
     images: {
         remotePatterns: [
