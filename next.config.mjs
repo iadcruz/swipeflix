226dfee4b151abc0e39787a681aedf7ded33a458
iadcruz <iadcruzsa21@gmail.com>
2025-08-01 22:13:15 -0400

    Config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+    reactStrictMode: true,
     async headers() {
-        return [
+    return [
         {
-            source: '/(.*)',
-            headers: [
+        source: "/(.*)",
+        headers: [
             {
-                key: 'X-Frame-Options',
-                value: 'ALLOWALL',
-            },
-            {
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors *",
-            },
-            ],
-        },
-        ];
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://iadcruz.github.io"
+            }
+        ]
+        }
+    ];
     },
-    
+
     images: {
         remotePatterns: [
             {
